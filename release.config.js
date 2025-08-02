ebe65639f42c89eb8666935040b87d1ce81ab4bc
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-08-02 14:48:30 +0900

    ci: use GitHub App for semantic-release to enable direct push (#50)

--- release.config.js
+++ release.config.js
@@ -15,9 +15,8 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
